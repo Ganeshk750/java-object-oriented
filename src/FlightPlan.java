5050c193968972af58c3411055c24ccfcc2ad41a
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-04-13 22:38:35 +0530

    # Testing FlightPlan class in Main class

--- src/FlightPlan.java
+++ src/FlightPlan.java
@@ -14,36 +14,47 @@ public class FlightPlan {
     LocalDateTime departureTime;
     List<String> route;
 
-    private FlightPlan(){
+    private FlightPlan() {
         System.out.println("FlightPlan()");
         this.id = UUID.randomUUID().toString();
     }
 
-    FlightPlan(String departure, String destination){
-       this();
-       if(departure == null || destination == null){
-           throw new IllegalArgumentException();
-       }
-        System.out.println(String.format("FlightPlan(%s, %s)", departure, destination));
-       this.departure = departure;
-       this.destination = destination;
+    FlightPlan(String departure, String destination) {
+        this();
+
+        if (departure == null || destination == null) {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println(String.format("FlightPlan( %s, %s)", departure, destination));
+
+        this.departure = departure;
+        this.destination = destination;
     }
 
     FlightPlan(String departure, String destination, LocalDateTime departureTime, List<String> route) {
         this(departure, destination);
-        if(departureTime == null || route == null){
-            System.out.println(String.format("FlightPlan(%s, %s, %s, %s)", departure, destination, departureTime.toLocalTime(), route.toString()));
+
+        if (departureTime == null || route == null) {
+            throw new IllegalArgumentException();
         }
+
+        System.out.println(String.format("FlightPlan( %s, %s, %s, %s)",
+                departure,
+                destination,
+                departureTime.toLocalTime(),
+                route.toString()));
+
         this.route = route;
         this.departureTime = departureTime;
     }
 
     public void print() {
-        String msg =  "{" +
+        String msg = "{" +
                 "id='" + this.id + '\'' +
-                ", departure='" + this.departure + '\'' +
+                "departure='" + this.departure + '\'' +
                 ", destination='" + this.destination + '\'' +
-                ", departureTime=" + this.departureTime +
+                ", departureDateTime=" + this.departureTime +
                 ", route=" + this.route +
                 '}';
         System.out.println(msg);
